85ce98adbc9500a676f273f7a831a43f49367d3c
Liam Gallagher <ljpgallagher@xtra.co.nz>
2022-09-09 15:47:22 +1200

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,34 @@
 #[macro_use]
 extern crate rocket;
+use jammdb::DB;
 use rocket::serde::json::Json;
-use rusqlite::Connection;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use uuid::Uuid;
+
+const DATABASE: &str = "/data.db";
 
 #[launch]
 fn rocket() -> _ {
-    let db_connection = Connection::open("data.sqlite").unwrap();
-    db_connection
-        .execute(
-            "create table if not exists messages (
-                id integer primary key,
-                user varchar(64) not null,
-                body varchar(64) not null
-            );",
-            rusqlite::NO_PARAMS,
-        )
-        .unwrap();
-
-    rocket::build().mount("/", routes![index, get, send])
+    rocket::build()
+        .mount("/message", routes![send, get])
 }
 
-#[get("/")]
-fn index() -> String {
-    "Welcome to my api!".to_string()
+#[post("/<room>/<user>", format = "json", data = "<message>")]
+fn send(room: u128, user: u128, message: Json<String>) {
+    let db = DB::open(MESSAGE_DATABASE).expect("Failed to open message database");
+    let mut tx = db.tx(true);
+
+    let 
 }
 
-#[get("/message")]
-fn get() -> Result<Json<Vec<Message>>, String> {
-    let db_connection = match Connection::open("data.sqlite") {
-        Ok(connection) => connection,
-        Err(_) => return Err("Failed to connect to database".into()),
-    };
-
-    let mut statement = match db_connection.prepare("select id, user, body from messages;") {
-        Ok(statement) => statement,
-        Err(_) => return Err("Failed to select messages from database".into()),
-    };
-
-    let results = statement.query_map(rusqlite::NO_PARAMS, |row| {
-        Ok(Message {
-            id: row.get(0)?,
-            user: row.get(1)?,
-            body: row.get(2)?,
-        })
-    });
-
-    match results {
-        Ok(rows) => {
-            let collection: rusqlite::Result<Vec<Message>> = rows.collect();
-            match collection {
-                Ok(items) => Ok(Json(items)),
-                Err(_) => return Err("Failed to collect items".into()),
-            }
-        },
-        Err(_) => return Err("Failed to fetch messages".into()),
-    }
+#[get("/<room>/<count>")]
+fn get(room: u128, count: u8) {
+    
 }
 
-#[post("/message", format = "json", data = "<item>")]
-fn send(item: Json<String>) -> Result<Json<StatusMessage>, String> {
-    let db_connection = match Connection::open("data.sqlite") {
-        Ok(connection) => connection,
-        Err(_) => {
-            return Err(String::from("Failed to connect to database"));
-        }
-    };
-
-    let mut statement =
-        match db_connection.prepare(&format!("insert into messages (id, user, body) values (null, {}, {});", "usererr", "bodyodyody")) {
-            Ok(statement) => statement,
-            Err(e) => return Err(e.to_string()),
-        };
-    let results = statement.execute(&[&item.0]);
-
-    match results {
-        Ok(rows_affected) => Ok(Json(StatusMessage {
-            message: format!("{} rows inserted!", rows_affected),
-        })),
-        Err(e) => Err(e.to_string()),
-    }
-}
-
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 struct Message {
-    id: i64,
-    user: String,
+    user: u128,
     body: String,
-}
-
-#[derive(Serialize)]
-struct StatusMessage {
-    message: String,
 }
